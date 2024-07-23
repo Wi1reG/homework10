464d8c403e1fd394a4c7eee1d8aa79cc33aef894
Wi1reG <anikin-andrey1@mail.ru>
2024-07-23 19:55:38 +0300

    исправления

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void chekYear(int year) {
+    public static void checkYear(int year) {
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println(year + " год является високосным");
         } else if (year != 0) {
@@ -9,21 +11,22 @@ public class Main {
         }
     }
 
-    public static void chekOS(int clientOs, int clientDeviceYear) {
-        if (clientOs == 0 && clientDeviceYear >= 2015) {
+    public static void checkOS(int clientOs) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientOs == 0 && currentYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOs == 0 && clientDeviceYear < 2015) {
+        } else if (clientOs == 0 && currentYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
 
-        if (clientOs == 1 && clientDeviceYear >= 2015) {
+        if (clientOs == 1 && currentYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOs == 1 && clientDeviceYear < 2015) {
+        } else if (clientOs == 1 && currentYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
     }
 
-    public static void chekDistance(int deliveryDistance) {
+    public static void checkDistance(int deliveryDistance) {
         int day = 1;
         if (deliveryDistance < 20) {
             System.out.println("На доставку карты потребуется " + day + " день");
@@ -40,8 +43,8 @@ public class Main {
 
 
     public static void main(String[] args) {
-        chekYear(2020);
-        chekOS(0, 2001);
-        chekDistance(95);
+        checkYear(2020);
+        checkOS(0);
+        checkDistance(95);
     }
 }
